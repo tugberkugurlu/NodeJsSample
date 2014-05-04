6e9a2c9b01655843f741280b1c55b76ea7e889b9
tugberkugurlu <tugberkugurlu@gmail.com>
2014-05-04 19:09:08 +0300

    lastest changes to add console logging

--- index.js
+++ index.js
@@ -19,6 +19,7 @@
 
     // #### Less Integration
     // TODO: Add resources here...
+
 var http = require('http'),
     express = require('express'),
     app = express();
@@ -27,11 +28,25 @@ app.set('port', process.env.PORT || 3000);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
 
-app.get('/hello.txt', function(req, res) {
+// logger middleware...
+app.use(function(req, res, next) {
+  console.log('%s %s', req.method, req.url);
+  next();
+});
+
+app.get('/', function (req, res) {
+    res.send('Hello World!');
+});
+
+app.get('/help', function (req, res) {
+    res.send('Nope.. nothing to see here');
+});
+
+app.get('/hello.txt', function (req, res) {
     res.set('Content-Type', 'text/plain');
     res.send('Hello World');
 });
 
-http.createServer(app).listen(app.get('port'), function() {
+http.createServer(app).listen(app.get('port'), function () {
     console.log('Express server listening on port ' + app.get('port'));
 });
